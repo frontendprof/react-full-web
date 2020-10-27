1f9bc9b0c567c81e4222495aeefee720e82bb414
frontendprof <abdumaliksharipov@gmail.com>
2020-10-28 00:47:24 +0500

    Getting media files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+import Navbar from "./components/Navbar";
+
 import './App.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Peace</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact/>
+        </Switch>
+
+      </Router>
+    </>
   );
 }
 
